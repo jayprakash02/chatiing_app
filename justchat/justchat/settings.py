41ecbb44c4fd59082d5469a3c1dd860a1687d62d
jayprakash02 <starjayp02@gmail.com>
2020-03-06 12:55:21 +0530

    test cases were made

--- justchat/justchat/settings.py
+++ justchat/justchat/settings.py
@@ -79,6 +79,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST':{
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
+        # chromedriver --url-base=/wd/hub
+        # for enabling crome-driver local host
+        # refer -- https://tecadmin.net/setup-selenium-chromedriver-on-ubuntu/
+
     }
 }
 
